2537564fbbdbae3a754b50079a10489d633ca3c5
Sabrina Cosolo <ziayeye@ziayeye.it>
2016-11-08 17:40:47 +0100

    The test 2 committments

--- TestDotnet/Program.cs
+++ TestDotnet/Program.cs
@@ -11,6 +11,10 @@ namespace TestDotnet
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello dolls");
+			Console.WriteLine("You know who I am");
+
+			Console.WriteLine("You don't know where I am");
+			Console.WriteLine("And you'll never see me coming");
 			Console.ReadLine();
 		}
 	}
